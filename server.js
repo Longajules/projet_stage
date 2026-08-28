afc0ce26517e86076f026ca2aceaa0c3bd6ae0b8
Longajules <judonite60@gmail.com>
2026-08-28 12:39:04 +0200

    mise a jour logo

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Sert tous les fichiers du dossier "public"
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route principale (optionnelle car Express sert déjà index.html automatiquement)
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
